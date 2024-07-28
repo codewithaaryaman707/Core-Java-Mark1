59d77d69c084900329f91a7d06ae25a1f1c63861
Aditya S Kelaskar <kelaskaraditya1@gmail.com>
2024-07-29 01:30:56 +0530

    Patterns done

---
+++ src/Dsa/Patterns/PatternsMark1.java
@@ -0,0 +1,51 @@
+package Dsa.Patterns;
+import java.util.Scanner;
+public class PatternsMark1 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int count=1;
+        System.out.println("Enter no of Rows");
+        int rows=s.nextInt();
+//        System.out.println("Enter no of Cols");
+//        int cols=s.nextInt();
+//        for(int i=0;i<rows;i++)
+//        {
+//            for(int j=0;j<cols;j++)
+//                System.out.print("*");
+//            System.out.println();
+//        }
+//        for(int i=0;i<rows;i++){
+//            for(int j=0;j<i+1;j++)
+//                System.out.print("*");
+//            System.out.println();
+//        }
+//        for(int i=0;i<rows;i++){
+//            for(int j=0;j<rows-i;j++)
+//                System.out.print("*");
+//            System.out.println();
+//        }
+//        for(int i=0;i<rows;i++)
+//        {
+//            for(int j=0;j<i+1;j++)
+//                System.out.print(count++);
+//            System.out.println();
+//            count=1;
+//        }
+//        for(int i=0;i<rows;i++)
+//        {
+//            for(int k=0;k<rows-i;k++)
+//                System.out.print(" ");
+//            for(int j=0;j<2*i+1;j++)
+//                System.out.print("*");
+//            System.out.println();
+//        }
+//        for(int i=0;i<rows;i++)
+//        {
+//            for(int j=0;j<i;j++)
+//                System.out.print(" ");
+//            for(int k=0;k<rows-i;k++)
+//                System.out.print("* ");
+//            System.out.println();
+//        }
+    }
+}
